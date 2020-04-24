62c8f73b78a09f1c13663f172e43b84d461d794c
Hal Peters <geohalbert@gmail.com>
2020-04-23 22:30:19 -0500

    create navigation, home screen

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,41 @@
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+// screens
+import HomeScreen from "../screens/HomeScreen";
+
+// default style
+const defaultStyle = {
+  headerTintColor: "#fff",
+  headerStyle: {
+    backgroundColor: "#ccebff"
+  },
+  headerTitleStyle: {
+    textAlign: "center",
+    flexGrow: 1,
+    alignSelf: "center"
+  }
+};
+
+// initial app stack
+const AppStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: "Tasks",
+      ...defaultStyle
+    }
+  }
+});
+
+// app navigation logic
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      App: AppStack
+    },
+    {
+      initialRouteName: "App"
+    }
+  )
+);
