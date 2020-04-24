005a68a4e9e4347d153a66cd886b3d4b38c76170
Hal Peters <geohalbert@gmail.com>
2020-04-24 07:59:50 -0500

    created store, app using provider

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import AppNavigator from "./navigation/AppNavigator";
+import { Provider } from "react-redux";
+
+import store from "./store/task.store"; //Import the store
 
 export default function App() {
   const AppContainer = createAppContainer(AppNavigator);
-  return <AppContainer />;
+  return (
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
+  );
 }
